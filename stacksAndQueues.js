d5a263f209ee7d5b97afca197ad788a573a20c85
Jonathan Puc <jonpucxyz@Jonathans-MacBook-Pro.local>
2017-11-29 21:30:01 +1100

    create stack instance with an object as storage

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -1,34 +1,72 @@
-var Stack = function() {
-  this.storage = "";
-};
+// // Creating a Stack data structure with a string
+
+// function Stack() {
+//   this.storage = "";
+// }
+
+// Stack.prototype.push = function(val) {
+//   // stack frames will be separated by ---
+//   this.storage = this.storage.concat(`---${val}`);
+// };
+
+// Stack.prototype.pop = function() {
+//   // temp array to contain stack frames without separator
+//   var arr = this.storage.split("---");
+//   // remove the last stack frame and save it into popped to return
+//   var popped = arr.splice([arr.length - 1]);
+//   // reassign storage with updated stack frames with separator
+//   this.storage = arr.join("---");
+//   return popped;
+// };
+
+// Stack.prototype.size = function() {
+//   var size =
+//     this.storage === ""
+//       ? // simply return 0 if storage is empty
+//         0
+//       : // expose stack frames without seperator, filtering out any unnecessary data like trim() but with an array
+//         // and assign its length to variable
+//         this.storage.split("---").filter(val => val !== "").length;
+//   return size;
+// };
+
+// var myWeeklyMenu = new Stack();
+
+// myWeeklyMenu.push("RedBeans");
+
+// Creating a Stack data structure with a string
+
+function Stack(capacity) {
+  this._capacity = capacity || Infinity;
+  this._storage = {};
+  this._count = 0;
+}
 
 Stack.prototype.push = function(val) {
-  // stack frames will be separated by ---
-  this.storage = this.storage.concat(`---${val}`);
+  if (this._count < this._capacity) {
+    this._storage[this._count++] = val;
+    return this._count;
+  } else {
+    alert("Full capacity");
+  }
 };
 
 Stack.prototype.pop = function() {
-  // temp array to contain stack frames without separator
-  var arr = this.storage.split("---");
-  // remove the last stack frame and save it into popped to return
-  var popped = arr.splice([arr.length - 1]);
-  // reassign storage with updated stack frames with separator
-  this.storage = arr.join("---");
+  debugger;
+  var value = this._storage[--this._count];
+  delete this._storage[this._count];
+  if (this._count < 0) {
+    this._count = 0;
+  }
+  return value;
+};
 
-  return popped;
+Stack.prototype.peek = function() {
+  return this._storage[this._count - 1];
 };
 
 Stack.prototype.size = function() {
-  var size =
-    this.storage === ""
-      ? // simply return 0 if storage is empty
-        0
-      : // expose stack frames without seperator, filtering out any unnecessary data like trim() but with an array
-        // and assign its length to variable
-        this.storage.split("---").filter(val => val !== "").length;
-  return size;
+  return this._count;
 };
 
 var myWeeklyMenu = new Stack();
-
-myWeeklyMenu.push("RedBeans");
